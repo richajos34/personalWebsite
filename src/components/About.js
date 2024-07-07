d5650353eb065b39ca1d0aaebf4dc1683344b40f
Richa Jos <richajos34@berkeley.edu>
2024-07-06 19:19:04 -0700

    color gradient

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Box, Flex, Heading, Text, Button, IconButton, useColorMode } from '@chakra-ui/react';
+import { Box, Flex, Heading, Text, Button, IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { FaGithub, FaLinkedin, FaEnvelope, FaFileDownload } from 'react-icons/fa';
-import profileImage from '../assets/profilePhoto.png'; // Add your profile image here
 
 function About() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const gradientOverlay = useColorModeValue(
+    'linear(to-b, rgba(255, 255, 255, 0), rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.7))',
+    'linear(to-b, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7))'
+  );
 
   return (
     <Box
       id="about"
-      bgImage="url('https://st4.depositphotos.com/1000423/24178/i/450/depositphotos_241782602-stock-photo-privacy-safety-concept.jpg')"
-      bgSize="cover"
-      bgPosition="center"
+      position="relative"
       height="100vh"
       display="flex"
       alignItems="center"
@@ -19,18 +20,36 @@ function About() {
       color="white"
       textAlign="center"
     >
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        right="0"
+        bottom="0"
+        bgColor={'gray.700'}
+        //bgImage="url('https://t3.ftcdn.net/jpg/07/73/17/96/360_F_773179623_rMQ1L1EO0XmFm6pM1o51lO6SXyPDMktH.jpg')"
+        bgSize="cover"
+        bgPosition="center"
+        zIndex="-1"
+        _after={{
+          content: '""',
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          right: '0',
+          bottom: '0',
+          bgGradient: gradientOverlay,
+        }}
+      />
       <Flex direction="column" align="center" bg="rgba(0, 0, 0, 0.7)" p={10} borderRadius="md">
-        <IconButton
-          aria-label="Toggle Light Mode"
-          icon={colorMode === 'light' ? '🌙' : '☀️'}
-          onClick={toggleColorMode}
-          mb={4}
-        />
-        <Heading as="h1" size="2xl" mb={4}>
-          I AM RICHA JOS
+        <Heading as="h1" size="2xl" mb={3}>
+          Hello World,
         </Heading>
-        <Text fontSize="lg" mb={6} maxW="600px">
-          I am a full stack developer with a passion for building scalable and responsive web applications. Currently pursuing a Bachelor of Science in Electrical Engineering and Computer Science, I have honed my skills in various programming languages, most notibly Java, Python, and C. My projects, reflect my commitment to leveraging technology to solve real-world problems and enhance the experiences of my peers around me. Beyond coding, I have a deep interest in artificial intelligence, exploring how AI can be integrated into everyday applications. When i'm not busy coding, I enjoy playing badminton, tennis, painting, and cooking!
+        <Text fontWeight='bold' fontSize="2xl" maxW="600px" mb={14}>
+          I'm Richa!
+        </Text>
+        <Text fontSize="lg" maxW="600px" mb={6}>
+          I am a full stack developer with a passion for building scalable and responsive web applications. Currently pursuing a Bachelor of Science in Electrical Engineering and Computer Science, I have honed my skills in various programming languages, most notably Java, Python, and C. My projects reflect my commitment to leveraging technology to solve real-world problems and enhance the experiences of my peers around me. Beyond coding, I have a deep interest in artificial intelligence, exploring how AI can be integrated into everyday applications. When I'm not busy coding, I enjoy playing badminton, tennis, painting, and cooking!
         </Text>
         <Button colorScheme="teal" size="lg" mb={6}>
           Learn More
@@ -39,7 +58,7 @@ function About() {
           <IconButton as="a" href="https://github.com/richajos34" icon={<FaGithub />} aria-label="GitHub" m={2} />
           <IconButton as="a" href="https://www.linkedin.com/in/richa-jos/" icon={<FaLinkedin />} aria-label="LinkedIn" m={2} />
           <IconButton as="a" href="mailto:richajos34@berkeley.edu" icon={<FaEnvelope />} aria-label="Email" m={2} />
-          <IconButton as="a" href="/Users/richajos/Desktop/Projects/personalWebsite/src/assets/Resume_Richa_Jos.pdf" aria-label="Download Resume" icon={<FaFileDownload />} m={2} download/>
+          <IconButton as="a" href="/Users/richajos/Desktop/Projects/personalWebsite/src/assets/Resume_Richa_Jos.pdf" aria-label="Download Resume" icon={<FaFileDownload />} m={2} download />
         </Flex>
       </Flex>
     </Box>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Heading, Text, Image, Flex, useColorModeValue } from '@chakra-ui/react';
+import { Box, Heading, Text, Image, Flex, Link, Button, useColorModeValue } from '@chakra-ui/react';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 import Slider from 'react-slick';
 import springLogo from '../assets/Spring_Framework.png';
 import javaLogo from '../assets/java.png';
@@ -11,10 +12,11 @@ import python from '../assets/python.png';
 import nodejs from '../assets/nodejs.png';
 import reactLogo from '../assets/react.png';
 import docker from '../assets/docker.png';
+import notium from '../assets/notium.png'
 import '../css/projects.css';
 
 function Projects() {
-  const cardBg = useColorModeValue('white', 'gray.800')
+  const cardBg = useColorModeValue('white', 'gray.800');
 
   const settings = {
     dots: true,
@@ -25,7 +27,37 @@ function Projects() {
     centerMode: true,
     centerPadding: '0',
   };
-  
+
+  const projectData = [
+    {
+      title: 'SafeZone',
+      description: "Interactive map application used for visualizing UC Berkeley's Crime data.",
+      technologies: [springLogo, javaLogo, javaScriptLogo, mongoDB, html, css],
+      githubLink: 'https://github.com/your-repo/safezone',
+      images: '../assets/safezone.png' // Add the path to your project image
+    },
+    {
+      title: 'Cricket Visualizer',
+      description: 'A simulation of a cricket game with optimal cricket passing algorithms and data structures.',
+      technologies: [python],
+      githubLink: 'https://github.com/your-repo/cricket-visualizer',
+      images: '../assets/cricket-visualizer.png' // Add the path to your project image
+    },
+    {
+      title: 'Video Convolution Processor',
+      description: 'Optimized 2D convolutions for video processing to achieve significant speedups.',
+      technologies: [docker, nodejs, mongoDB, reactLogo, javaScriptLogo],
+      githubLink: 'https://github.com/your-repo/video-convolution-processor',
+      image: '../assets/video-convolution-processor.png' // Add the path to your project image
+    },
+    {
+      title: 'Notium',
+      description: 'A full-stack replica of the popular note-taking website Notion.',
+      technologies: [docker, nodejs, mongoDB, reactLogo, javaScriptLogo],
+      githubLink: 'https://github.com/your-repo/notium',
+      images: [notium] // Add the path to your project image
+    }
+  ];
 
   return (
     <Box id="projects" textAlign="center" py={10} px={6}>
@@ -33,80 +65,31 @@ function Projects() {
         Projects
       </Heading>
       <Slider {...settings}>
-        <Box p={4} className="flip-card">
-          <Box className="flip-card-inner">
-            <Box className="flip-card-front" p={4}>
-              <Heading size="md" mt={4}>SafeZone</Heading>
-              <Text mt={4}>Interactive map application used for visualizing UC Berkeley's Crime data.</Text>
-              <Flex mt={4} justifyContent="center" alignItems="center">
-                <Image src={springLogo} alt="Spring Logo" boxSize="40px" mx={2} />
-                <Image src={javaLogo} alt="Java Logo" boxSize="40px" mx={2} />
-                <Image src={javaScriptLogo} alt="JavaScript Logo" boxSize="40px" mx={2} />
-                <Image src={mongoDB} alt="Mongo DB Logo" boxSize="40px" mx={2} />
-                <Image src={html} alt="HTML Logo" boxSize="40px" mx={2} />
-                <Image src={css} alt="CSS Logo" boxSize="40px" mx={2} />
-              </Flex>
-            </Box>
-            <Box className="flip-card-back" p={4}>
-              <Heading size="md" mt={4}>SafeZone</Heading>
-              <Text mt={4}>Detailed information about the SafeZone project, including its features and technologies used.</Text>
-            </Box>
-          </Box>
-        </Box>
-        <Box p={4} className="flip-card">
-          <Box className="flip-card-inner">
-            <Box className="flip-card-front" p={4}>
-              <Heading size="md" mt={4}>Cricket Visualizer</Heading>
-              <Text mt={4}>A simulation of a cricket game with optimal cricket passing algorithms and data structures.</Text>
-              <Flex mt={4} justifyContent="center" alignItems="center">
-                <Image src={python} alt="Python Logo" boxSize="40px" mx={2} />
-              </Flex>
-            </Box>
-            <Box className="flip-card-back" p={4}>
-              <Heading size="md" mt={4}>Cricket Visualizer</Heading>
-              <Text mt={4}>Detailed information about the Cricket Visualizer project, including its features and technologies used.</Text>
+        {projectData.map((project, index) => (
+          <Box key={index} p={4} className="flip-card">
+            <Box className="flip-card-inner">
+              <Box className="flip-card-front" p={4}>
+                <Heading size="md" mt={4}>{project.title}</Heading>
+                <Image src={project.images} alt={`${project.title} Image`} align="center" borderRadius="md" mb={4} width="20%" objectFit="cover"/> {/* Project Image */}
+                <Text mt={4}>{project.description}</Text>
+                <Flex mt={4} justifyContent="center" alignItems="center">
+                  {project.technologies.map((tech, techIndex) => (
+                    <Image key={techIndex} src={tech} alt="Technology Logo" boxSize="40px" mx={2} />
+                  ))}
+                </Flex>
+              </Box>
+              <Box className="flip-card-back" p={4}>
+                <Heading size="md" mt={4}>{project.title}</Heading>
+                <Text mt={4}>Detailed information about the {project.title} project, including its features and technologies used.</Text>
+                <Link href={project.githubLink} isExternal mt={4} _hover={{ textDecoration: 'none' }}>
+                  <Button rightIcon={<FaExternalLinkAlt />} colorScheme="teal" variant="outline" mt={4} p={2}>
+                    Source
+                  </Button>
+                </Link>
+              </Box>
             </Box>
           </Box>
-        </Box>
-        
-        <Box p={4} className="flip-card">
-          <Box className="flip-card-inner">
-            <Box className="flip-card-front" p={4}>
-              <Heading size="md" mt={4}>Video Convolution Processor</Heading>
-              <Text mt={4}>Optimized 2D convolutions for video processing to achieve significant speedups.</Text>
-              <Flex mt={4} justifyContent="center" alignItems="center">
-                <Image src={docker} alt="Docker Logo" boxSize="40px" mx={2} />
-                <Image src={nodejs} alt="Node.js Logo" boxSize="40px" mx={2} />
-                <Image src={mongoDB} alt="Mongo DB Logo" boxSize="40px" mx={2} />
-                <Image src={reactLogo} alt="React Logo" boxSize="40px" mx={2} />
-                <Image src={javaScriptLogo} alt="JavaScript Logo" boxSize="40px" mx={2} />
-              </Flex>
-            </Box>
-            <Box className="flip-card-back" p={4}>
-              <Heading size="md" mt={4}>Notium</Heading>
-              <Text mt={4}>Detailed information about the Notium project, including its features and technologies used.</Text>
-            </Box>
-          </Box>
-        </Box>
-        <Box p={4} className="flip-card">
-          <Box className="flip-card-inner">
-            <Box className="flip-card-front" p={4}>
-              <Heading size="md" mt={4}>Notium</Heading>
-              <Text mt={4}>A full-stack replica of the popular note-taking website Notion.</Text>
-              <Flex mt={4} justifyContent="center" alignItems="center">
-                <Image src={docker} alt="Docker Logo" boxSize="40px" mx={2} />
-                <Image src={nodejs} alt="Node.js Logo" boxSize="40px" mx={2} />
-                <Image src={mongoDB} alt="Mongo DB Logo" boxSize="40px" mx={2} />
-                <Image src={reactLogo} alt="React Logo" boxSize="40px" mx={2} />
-                <Image src={javaScriptLogo} alt="JavaScript Logo" boxSize="40px" mx={2} />
-              </Flex>
-            </Box>
-            <Box className="flip-card-back" p={4}>
-              <Heading size="md" mt={4}>Notium</Heading>
-              <Text mt={4}>Detailed information about the Notium project, including its features and technologies used.</Text>
-            </Box>
-          </Box>
-        </Box>
+        ))}
       </Slider>
     </Box>
   );
